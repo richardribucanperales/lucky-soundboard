6f1fed478547ef610b031170b2689d806be131db
Richard Ribucan-Perales <30878020+richardribucanperales@users.noreply.github.com>
2019-05-05 23:31:03 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function draven(){
+	var draven = new Audio("./assets/sounds/draven_scream.mp3")
+	draven.play()
+}
+function excuse(){
+	var excuse = new Audio("./assets/sounds/excuse_me_what.mp3")
+	excuse.play()
+}
+function hohoho(){
+	var hohoho = new Audio("./assets/sounds/hohoho.mp3")
+	hohoho.play()
+}
+function itsyaboi(){
+	var itsyaboi = new Audio("./assets/sounds/its_ya_boi.mp3")
+	itsyaboi.play()
+}
+function leapfrog(){
+	var leapfrog = new Audio("./assets/sounds/leapfrog_use_it.mp3")
+	leapfrog.play()
+}
+function tuffguy(){
+	var tuffguy = new Audio("./assets/sounds/ooo_tuff_guy.mp3")
+	tuffguy.play()
+}
